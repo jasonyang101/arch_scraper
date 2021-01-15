7a563b904f8c82d85057267b9b2bafb7af07a5dc
jasonyang101 <jyang98il@gmail.com>
2021-01-15 10:09:57 -0800

    jy: adding address_search class, fixing up indeed_scraper

--- src/address_search.py
+++ src/address_search.py
@@ -1,39 +1,66 @@
 import requests
 from pprint import pprint
-from google import google
+import urllib
+import constant
 # https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY
 
-api_key_file = open('../creds/google_api_key.txt')
-api_key = api_key_file.read()
 
-def construct_url(search_string):
-    url = [ 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?' ]
-    params = {
-        'query': search_string.replace(' ', '%20'),
-        'inputtype': 'textquery',
-        'fields': 'formatted_address',
-        'key': api_key
-    }
-    for param, param_val in params.items():
-        url.append(param+'='+param_val)
-    return '&'.join(url)
+class AddressSearcher:
+    constant.ADDR = 'formatted_address'
+    constant.CANDIDATES = 'candidates'
+
+    def __init__(self, search_strings=[]):
+        api_key_file = open('../creds/google_api_key.txt')
+        self.api_key = api_key_file.read()
+        self.search_strings = search_strings
+
+    def set_search_strings(self, search_strings):
+        self.search_strings = search_strings
+
+    def construct_url(self, search_string):
+        url = [ 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?' ]
+        params = {
+            'input': urllib.parse.quote(search_string),
+            'inputtype': 'textquery',
+            'fields': 'formatted_address',
+            'key': self.api_key
+        }
+        for param, param_val in params.items():
+            url.append(param+'='+param_val)
+        return '&'.join(url)
+
+    # leaving this here but unsure if it will be used
+    def text_search_url(self, search_string):
+        url = [ 'https://maps.googleapis.com/maps/api/place/textsearch/json?' ]
+        params = {
+            'query': urllib.parse.quote(search_string),
+            'key': self.api_key
+        }
+        for param, param_val in params.items():
+            url.append(param+'='+param_val)
+        return '&'.join(url)
+
+    def run_searches(self):
+        search_output = {}
+        failed_searches = []
+        for search in self.search_strings:
+            find_place_url = self.construct_url(search)
+            print('querying from', find_place_url)
+            r = requests.get(find_place_url)
+            response_json = r.json()
+            if response_json['status'] != 'OK':
+                failed_searches.append(search)
+                continue
+            # pull the best candidate
+            candidates = response_json['candidates']
+            addr = candidates[0]['formatted_address']
+            search_output[search] = addr
+        return (search_output, failed_searches)
 
 
-def text_search_url(search_string):
-    url = [ 'https://maps.googleapis.com/maps/api/place/textsearch/json?' ]
-    params = {
-        'query': search_string.replace(' ', '+'),
-        # 'inputtype': 'textquery',
-        # 'fields': 'formatted_address',
-        'key': api_key
-    }
-    for param, param_val in params.items():
-        url.append(param+'='+param_val)
-    return '&'.join(url)
+
 
 # find_place_url = construct_url('Healthy Smiles of La Grange La Grange, IL 60525')
-sample = "Healthy Smiles of La Grange La Grange, IL 60525"
-text_search_url = text_search_url(sample)
-search_results = google.search(sample, num_page)
-r = requests.get(text_search_url)
-pprint(r.json())
+# sample = "Healthy Smiles of La Grange La Grange, IL 60525"
+# address_search = AddressSearcher([sample])
+# address_search.run_searches()
--- src/indeed_scraper.py
+++ src/indeed_scraper.py
@@ -1,30 +1,39 @@
+import urllib
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from time import sleep
+import website_parser_common
+import random
 import constant
 
-class IndeedData:
-    def __init__(self, company_name, company_loc):
-        self.name = company_name
-        self.loc = company_loc
+class IndeedSearchData:
+    def __init__(self, company_name, company_loc, search_string):
+        self.company_name = company_name
+        self.company_loc = company_loc
+        self.search_string = search_string
 
 class IndeedWebScraper(object):
     # specific classes
     constant.DIV_COMPANY_CARD_CLASS = 'sjcl'
     constant.LOCATION_CLASS = 'location'
     constant.COMPANY_CLASS = 'company'
+    constant.NUM_PER_PAGE = 10
 
-    def __init__(self, url):
-        page = urlopen(url)
-        html_bytes = page.read()
-        html = html_bytes.decode('utf-8')
-        soup = BeautifulSoup(html, 'html.parser')
-        company_cards = soup.find_all(lambda tag: tag.name == 'div' and
-                                           tag.get('class') == [ constant.DIV_COMPANY_CARD_CLASS ])
-        self.data = self.parse_company_cards(company_cards)
+    def __init__(self, role, location):
+        print('starting a new scraper for indeed with role, location: ', role, location)
+        self.role = role
+        self.location = location
+        self.base_url = self.construct_base_url(role,location)
+        self.parser = website_parser_common.WebsiteParser()
+
+    def construct_base_url(self, role, location):
+        role_str = "q="+urllib.parse.quote(role)
+        loc_str = "l="+urllib.parse.quote(location)
+        radius = "radius=15"
+        return "https://www.indeed.com/jobs?" + '&'.join([role_str, loc_str, radius])
 
     def parse_company_cards(self, company_cards):
         card_data = []
-        print(len(company_cards))
         for i, cc in enumerate(company_cards):
             # print(serp_card)
             name, loc = None, None
@@ -33,11 +42,59 @@ class IndeedWebScraper(object):
             loc_html = cc.find_all(class_=constant.LOCATION_CLASS)
             if loc_html:
                 loc = loc_html[0].get_text(' ', strip=True)
-            card_data.append(IndeedData(name, loc))
-        for c in card_data:
-            print(c.name, c.loc)
+                start_paren_idx = loc.find('(')
+                if start_paren_idx != -1:
+                    loc = loc[:start_paren_idx]
+            search_string = name + ((' '+loc) if loc else '')
+            info = IndeedSearchData(name, loc, search_string)
+            card_data.append(info)
         return card_data
 
+    # get all of the cards from the html file
+    def get_cards_per_page(self, url):
+        soup = self.parser.get_soup_for_url(url)
+        page_cards = soup.find_all(lambda tag: tag.name == 'div' and
+                                            tag.get('class') == [ constant.DIV_COMPANY_CARD_CLASS ])
+        return page_cards
 
-url = "https://www.indeed.com/jobs?q=Dentist+Associate&l=Chicago%2C+IL"
-web_scraper = IndeedWebScraper(url)
+    # add to the set until you're done
+    def do_scrape(self):
+        finished, data = None, set()
+        url, iteration = self.base_url, 0
+        fails, all_infos = 2, []
+        while not finished:
+            finished = True
+            start_param = iteration*constant.NUM_PER_PAGE
+            if start_param:
+                url = self.base_url+"&start="+str(start_param)
+            page_cards = self.get_cards_per_page(url)
+            new_info = self.parse_company_cards(page_cards)
+            print('searching this url for new info: ', url)
+            count = 0
+            for info in new_info:
+                if info.search_string not in data:
+                    count += 1
+                    finished = False
+                    print('adding new entry', info.search_string)
+                    data.add(info.search_string)
+                    all_infos.append(info)
+            print('adding this many new informations to the output:', count)
+            iteration+=1
+            # sometimes if we get 0 answers out, if we query again, we'll get new answers
+            # so to combat this, we'll only quit out after getting 0 new entries 3 times total
+            if finished and fails > 0:
+                fails -= 1
+                finished = not finished
+            sleep(random.randint(3,10))
+        return all_infos
+
+    def scrape(self):
+        search_infos = []
+        infos = self.do_scrape()
+        for info in infos:
+            search_infos.append(IndeedSearchData(self.role, self.location, info))
+        return search_infos
+
+# url = "https://www.indeed.com/jobs?q=Dentist+Associate&l=Chicago%2C+IL"
+# web_scraper = IndeedWebScraper('Dental Associate','Chicago, IL')
+# web_scraper.scrape()
