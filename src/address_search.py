66ccb91defd4631d3331e1e0a7564099e2cb8ea5
jasonyang101 <jyang98il@gmail.com>
2021-01-15 11:07:14 -0800

    jy: adding parsing formatted address + location bias for query

--- src/address_search.py
+++ src/address_search.py
@@ -2,8 +2,9 @@ import requests
 from pprint import pprint
 import urllib
 import constant
-# https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY
 
+# sample URL
+# https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY
 
 class AddressSearcher:
     constant.ADDR = 'formatted_address'
@@ -19,10 +20,13 @@ class AddressSearcher:
 
     def construct_url(self, search_string):
         url = [ 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?' ]
+
+        # &locationbias=circle:2000@47.6918452,-122.2226413
         params = {
             'input': urllib.parse.quote(search_string),
             'inputtype': 'textquery',
             'fields': 'formatted_address',
+            'locationbias': 'circle:100000@41.8781,-87.623177',
             'key': self.api_key
         }
         for param, param_val in params.items():
@@ -53,6 +57,10 @@ class AddressSearcher:
                 continue
             # pull the best candidate
             candidates = response_json['candidates']
+            # search the next candidates if it doesn't include the right answer
+            best_candidate, i = candidates[0], 1
+            while i < len(candidates) and 'IL' not in best_candidate:
+                best_candidate, i = candidates[i], i+1
             addr = candidates[0]['formatted_address']
             search_output[search] = addr
         return (search_output, failed_searches)
@@ -61,6 +69,6 @@ class AddressSearcher:
 
 
 # find_place_url = construct_url('Healthy Smiles of La Grange La Grange, IL 60525')
-# sample = "Healthy Smiles of La Grange La Grange, IL 60525"
+# sample = "A to Z Dental, LLC Cicero, IL"
 # address_search = AddressSearcher([sample])
-# address_search.run_searches()
+# print(address_search.run_searches())
--- src/execute_scraping.py
+++ src/execute_scraping.py
@@ -16,7 +16,9 @@ class ScrapeExecutor:
         self.site = site
         if not os.path.exists('../results'):
             os.mkdir('../results')
-
+        # REFACTOR THIS WHEN WE HAVE MULTIPLE SITES
+        if os.path.exists('../results/output.csv'):
+            os.remove('../results/output.csv')
 
     def scrape(self):
         all_infos = set()
@@ -49,19 +51,33 @@ class ScrapeExecutor:
                 continue
             roles_for_info = [ role for role in constant.SEARCH_ROLES if role in self.role_data and info in self.role_data[role] ]
             roles_str = '+'.join(roles_for_info)
-            csv_output.append( [info.company_name, roles_str, address_dict[info.search_string]] )
+            parsed_addr = self.parse_formatted_address(address_dict[info.search_string])
+            csv_entry = [info.company_name, roles_str] + parsed_addr
+            csv_output.append(csv_entry)
         return csv_output
+
+    # example: 519 N Cass Ave Ste 401, Westmont, IL 60559, United States
+    def parse_formatted_address(self, addr):
+        addr_split = addr.split(',')
+        components = list(map(lambda s: s.strip(), addr_split))
+        # street=519 N Cass Ave Ste 401
+        street = components[0]
+        # split IL 60559 into [IL, 60559]
+        state_zip = components[2].split(' ')
+        # city=Westmont
+        city_state = components[1]+' '+state_zip[0]
+        return [street, city_state, state_zip[1]]
 
     def write_data(self, csv_output):
         m = 'a' if os.path.exists(constant.results_path) else 'w'
         with open(constant.results_path, m) as f:
             for data in csv_output:
-                data = map(lambda s: s.strip(), data)
+                data = map(lambda s: s.replace(',', '').strip(), data)
                 data_str = ', '.join(data) + '\n'
                 f.write(data_str)
 
     def after_run(self, csv_output):
-        print("Successfully written to results/output.csv")
+        print("Successfully written data output to results/output.csv")
         print("Total number of written entries:", len(csv_output))
 
     def search_infos(self, search_infos):
@@ -76,6 +92,7 @@ class ScrapeExecutor:
     def execute(self):
         all_infos = self.scrape()
         address_dict = self.search_infos(all_infos)
+
         csv_output = self.get_csv_output(all_infos, address_dict)
         self.write_data(csv_output)
         self.after_run(csv_output)
